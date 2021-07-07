bb2ea0fc320bbf3921669b6a1abce90cf2f6160d
Ravil Galeyev <dehasi@yandex.ru>
2021-07-07 23:07:19 +0200

    Refacotoring

--- src/test/java/medium/Task378Test.java
+++ src/test/java/medium/Task378Test.java
@@ -1,5 +1,8 @@
 package medium;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,12 +16,27 @@ class Task378Test {
             {1, 5, 9}, {10, 11, 13}, {12, 13, 15}
         }, 8);
 
-        assertEquals(result, 8);
+        assertEquals(result, 13);
+    }
+
+    @Test void test2() {
+        int result = solution.kthSmallest(new int[][] {
+            {-5}}, 1);
+
+        assertEquals(result, -5);
     }
 
     class Solution {
         public int kthSmallest(int[][] matrix, int k) {
-            return 1;
+
+            k -= 1;
+            List<Integer> ans = new ArrayList<>(matrix.length * matrix.length);
+            for (int i = 0; i < matrix.length; i++)
+                for (int j = 0; j < matrix[0].length; j++)
+                    ans.add(matrix[i][j]);
+
+            Collections.sort(ans);
+            return ans.get(k);
         }
     }
 }
