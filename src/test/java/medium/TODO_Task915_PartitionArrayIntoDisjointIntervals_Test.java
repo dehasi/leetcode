afe77463451748b1f2cfbca09f2d3898530c1aaa
Ravil Galeyev <dehasi@yandex.ru>
2021-07-26 13:00:58 +0200

    TASK-915

--- src/test/java/medium/TODO_Task915_PartitionArrayIntoDisjointIntervals_Test.java
+++ src/test/java/medium/TODO_Task915_PartitionArrayIntoDisjointIntervals_Test.java
@@ -34,11 +34,9 @@ public class TODO_Task915_PartitionArrayIntoDisjointIntervals_Test {
             rmin[n - 1] = nums[n - 1];
             for (int i = n - 2; i >= 0; --i)
                 rmin[i] = Math.min(rmin[i + 1], nums[i]);
-            // rmin[0] = Math.min(rmin[1], nums[0]);
             lmax[0] = nums[0];
-            for (int i = 1; i < n; ++i) {
+            for (int i = 1; i < n; ++i)
                 lmax[i] = Math.max(lmax[i - 1], nums[i]);
-            }
 
             for (int i = 0; i < n - 1; ++i) {
                 if (lmax[i] < rmin[i + 1])
