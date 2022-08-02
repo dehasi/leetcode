2752a1fb27ba4ea038de07537f9bcbcd4090cc48
Ravil Galeyev <dehasi@yandex.ru>
2022-08-02 21:18:39 +0200

    LC-378

--- src/test/java/leetcode/medium/Task378_KthSmallestElementSortedMatrix_Test.java
+++ src/test/java/leetcode/medium/Task378_KthSmallestElementSortedMatrix_Test.java
@@ -1,5 +1,7 @@
 package leetcode.medium;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,7 +28,54 @@ public class Task378_KthSmallestElementSortedMatrix_Test {
     static
     class Solution {
         public int kthSmallest(int[][] matrix, int k) {
-            return -42;
+            if (k == 1) return matrix[0][0];
+            class Triple {
+                final int i, j, val;
+
+                int val() {return val;}
+
+                Triple(int i, int j, int val) {
+                    this.i = i;
+                    this.j = j;
+                    this.val = val;
+                }
+            }
+            int n = matrix.length;
+            PriorityQueue<Triple> heap = new PriorityQueue<>(n, Comparator.comparing(Triple::val));
+
+            int kth = -1;
+            for (int j = 0; j < n; ++j) heap.add(new Triple(0, j, matrix[0][j]));
+
+            while (k-- > 0) {
+                var cur = heap.poll();
+                kth = cur.val;
+                int i = cur.i;
+                int j = cur.j + 1;
+                if (j < n)
+                    heap.add(new Triple(i, j, matrix[i][j]));
+                else {
+                    ++k;
+                }
+            }
+            return kth;
+        }
+
+        class Heap {
+            final int size;
+            final int[] vals;
+            int index = 0;
+
+            Heap(int size) {
+                this.size = size;
+                int pow = nextPowOfTwo(size);
+                vals = new int[2 * pow];
+            }
+
+            private static int nextPowOfTwo(int val) {
+                int pow = 1;
+                while (pow < val) pow *= 2;
+                return pow;
+            }
         }
     }
 }
