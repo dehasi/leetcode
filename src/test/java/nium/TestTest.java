d0e9307bde7c87ca898c642cac5f4328f3adf46b
Ravil Galeyev <dehasi@yandex.ru>
2023-05-10 22:19:18 +0200

    nium

--- src/test/java/nium/TestTest.java
+++ src/test/java/nium/TestTest.java
@@ -12,8 +12,27 @@ class TestTest {
         Assertions.assertEquals(5, count(3));
     }
 
-    static int count(int n){
-        if(n == 1) return 1;
+    int count_memo(int n) {
+        int[][] memo = new int[n + 1][n + 1];
+        return count_memo(0, 0, n, memo);
+    }
+
+    private int count_memo(int open, int closed, int n, int[][] memo) {
+        if (open == closed && closed == n) return 1;
+        if (memo[open][closed] > 0) return memo[open][closed];
+        int result = 0;
+
+        if (open < n) {
+            result += count_memo(open + 1, closed, n, memo);
+        }
+        if (closed < open) {
+            result += count_memo(open, closed + 1, n, memo);
+        }
+        return memo[open][closed] = result;
+    }
+
+    static int count(int n) {
+        if (n == 1) return 1;
 
         int[] dp = new int[n + 1];
         dp[0] = 1;
@@ -28,8 +47,8 @@ class TestTest {
 
 
     public static int count1(int n) {
-        if (n == 0)  return 0;
-        if (n == 1)  return 1;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
 
         int result = 0;
 
